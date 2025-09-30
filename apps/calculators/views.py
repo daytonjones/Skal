f16b38ef0fa516a792a7e7cf75f35a9fbb4e6b0b
Dayton Jones <jones.dayton@gmail.com>
2025-09-30 15:06:21 +0000

    Fixing SNA calculator for Mead and not wine, adding N-Factor for different yeast strains

--- apps/calculators/views.py
+++ apps/calculators/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from apps.yeast.views import YEASTS
 
 class CalculatorIndexView(TemplateView):
     template_name = "calculators/index.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["yeasts"] = YEASTS  # pass yeast list into template
+        return ctx
 
